2ace1d1d785f60eb04841313d2451286b5e83da0
BitsExploited <tspamiitesh@gmail.com>
2025-02-02 20:13:24 +0530

    Adding Insertion Sort

---
+++ insertion-sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  int arr[10] = {7,6,4,1,3,2,5,9,0,8};
+  int len = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 0; i < len; i++) 
+    printf("%d", *(arr + i));
+
+  for (int i = 0; i < len; i++) {
+    int temp = arr[i + 1];
+    int j = i + 1;
+
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  for (int i = 0; i < len; i++) 
+    printf("%d", *(arr + i));
+  
+  return 0;
+}
